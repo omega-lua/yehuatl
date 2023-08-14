0cbfe1d30a6304b2760165eb83a77bfdcf9350ae
omega-lua <oliver@gomer.ch>
2023-08-14 16:58:28 +0200

    Update mainmenu.lua
    added scene:hoverObj() and scene:interactWithObj function. Widgets made global, to be able to access them. scene.variables added in scene:show().

--- resources/scene/menu/mainmenu.lua
+++ resources/scene/menu/mainmenu.lua
@@ -5,11 +5,12 @@ local widget = require( "widget" )
 local composer = require( "composer" )
 local scene = composer.newScene()
 
+
 -- -----------------------------------------------------------------------------------
 -- Code outside of the scene event functions below will only be executed ONCE unless
 -- the scene is removed entirely (not recycled) via "composer.removeScene()"
 -- -----------------------------------------------------------------------------------
- 
+
 -- Function to handle button events
 local function handleButtonEvent( event )
     if (event.target.id == 'buttonPlay') then
@@ -30,18 +31,43 @@ local function handleButtonEvent( event )
     end
 end
  
- 
--- -----------------------------------------------------------------------------------
--- Scene event functions
--- -----------------------------------------------------------------------------------
- 
--- create()
-function scene:create( event )
- 
-    local sceneGroup = self.view
-    -- Code here runs when the scene is first created but has not yet appeared on screen
+function scene:hoverObj(nextObj)
+    for k,o in pairs(scene.objTable) do
+        if (k == nextObj) then
+            local params = {
+                time = 200,
+                transition = easing.outQuint,
+                xScale = 1.5,
+                yScale = 1.5,
+            }
+            
+            transition.to(o, params)
+        elseif (k== nil) then
+            
+        else
+            local params = {
+                time = 200,
+                transition = easing.outQuint,
+                xScale = 1,
+                yScale = 1,
+            }
+            transition.to(o, params)
+        end
+    end
+end
 
-    local buttonPlay = widget.newButton(
+function scene:interactWithObj(object)
+    if (object == "buttonPlay") then
+        library.handleSceneChange("resources.scene.menu.savemenu","menu", { effect = "fade", time = 400,})
+    elseif (object == "buttonSettings") then
+        library.handleSceneChange( "resources.scene.menu.settingsmenu", "menu",{ effect = "fade", time = 400,})
+    end
+end
+ 
+function scene:loadUI()
+    local sceneGroup = scene.view
+    
+    buttonPlay = widget.newButton(
         {
         left = ((display.viewableContentWidth+display.screenOriginX)/2)+200, --display.pixelWidth/2
         top = display.contentCenterY, -- Nicht ganz genau in Mitte, wegen Textfeld-Anchorpoint vorher: top = display.actualContentHeight/2
@@ -53,7 +79,7 @@ function scene:create( event )
         labelColor = { default={ 1, 1, 1 }, over={ 1, 1, 1, 0.5 } }
         }
     )
-    local buttonSettings = widget.newButton(
+    buttonSettings = widget.newButton(
         {
         left = ((display.viewableContentWidth+display.screenOriginX)/2)-200, --display.pixelWidth/2
         top = display.contentCenterY, -- Nicht ganz genau in Mitte, wegen Textfeld-Anchorpoint vorher: top = display.actualContentHeight/2
@@ -69,6 +95,18 @@ function scene:create( event )
     sceneGroup:insert( buttonPlay )
     sceneGroup:insert( buttonSettings )
 end
+-- -----------------------------------------------------------------------------------
+-- Scene event functions
+-- -----------------------------------------------------------------------------------
+ 
+-- create()
+function scene:create( event )
+ 
+    local sceneGroup = self.view
+    -- Code here runs when the scene is first created but has not yet appeared on screen
+
+    scene:loadUI()
+end
  
  
 -- show()
@@ -79,6 +117,19 @@ function scene:show( event )
  
     if ( phase == "will" ) then
         -- Code here runs when the scene is still off screen (but is about to come on screen)
+        
+        -- Scene variables have to be set here, otherwise some would be empty.
+        -- where the hover starts, normally in center of screen.
+        scene.currentObj = "center"
+        scene.objectMatrix = {
+            ["center"] = {nil, "buttonPlay", nil, "buttonSettings"},
+            ["buttonPlay"] = {nil, "buttonSettings", nil, "buttonSettings"},
+            ["buttonSettings"] = {nil, "buttonPlay", nil, "buttonPlay"},
+            }
+        scene.objTable = {["center"]=nil, ["buttonPlay"]=buttonPlay, ["buttonSettings"]=buttonSettings,}
+        runtime.currentScene = scene
+        runtime.currentSceneType = "menu"
+
  
     elseif ( phase == "did" ) then
         -- Code here runs when the scene is entirely on screen
