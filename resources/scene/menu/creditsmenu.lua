718c5c6d005898b525a430a2cc5637f6a1516dc1
omega-lua <oliver@gomer.ch>
2023-09-18 20:24:06 +0200

    Update creditsmenu.lua
    changes from the lib-redesign

--- resources/scene/menu/creditsmenu.lua
+++ resources/scene/menu/creditsmenu.lua
@@ -1,14 +1,19 @@
 -- Template from Solar2D-Guide: https://docs.coronalabs.com/guide/system/composer/index.html#template
 
+local lib = require( "resources.lib.lib" )
 local composer = require( "composer" )
- 
 local scene = composer.newScene()
  
 -- -----------------------------------------------------------------------------------
--- Code outside of the scene event functions below will only be executed ONCE unless
--- the scene is removed entirely (not recycled) via "composer.removeScene()"
+-- Scene variables
 -- -----------------------------------------------------------------------------------
- 
+
+scene.type = "menu"
+
+-- -----------------------------------------------------------------------------------
+-- Scene functions
+-- -----------------------------------------------------------------------------------
+
 function scene:loadUI()
     local sceneGroup = scene.view
 
