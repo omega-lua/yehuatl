762aee6e29a91aef5b66e3ae25f5215eb408f783
omega-lua <oliver@gomer.ch>
2023-09-24 21:32:54 +0200

    Update lib.lua
    removed old printTable function

--- resources/lib/lib.lua
+++ resources/lib/lib.lua
@@ -422,11 +422,11 @@ function inputdevice.set(deviceName, deviceType)
     local keybinds = {}
 
     if isSaved then
-        -- InputDevice is saved, get keybinds from settings
-        keybinds = lib.settings.table.controls.keybinds[deviceName]
         if not keybinds then
             print("ERROR: No keybinds are stored for this (saved) input device.")
+            return false
         end
+        keybinds = lib.settings.table.controls.keybinds[deviceName]
     else
         keybinds = lib.inputdevice.remember(deviceName, deviceType)
     end
@@ -751,87 +751,6 @@ function lib.print(node)
         print(node)
     end
 end
-
-function lib.printTable(node)
-    -- To print a table if needed. Source: https://gist.github.com/revolucas/dd1ecccfca32d558fddf70ddb39eb8a6
-    local cache, stack, output = {},{},{}
-    local depth = 1
-    local output_str = "{\n"
-  
-    while true do
-        local size = 0
-        for k,v in pairs(node) do
-            size = size + 1
-        end
-  
-        local cur_index = 1
-        for k,v in pairs(node) do
-            if (cache[node] == nil) or (cur_index >= cache[node]) then
-  
-                if (string.find(output_str,"}",output_str:len())) then
-                    output_str = output_str .. ",\n"
-                elseif not (string.find(output_str,"\n",output_str:len())) then
-                    output_str = output_str .. "\n"
-                end
-  
-                -- This is necessary for working with HUGE tables otherwise we run out of memory using concat on huge strings
-                table.insert(output,output_str)
-                output_str = ""
-  
-                local key
-                if (type(k) == "number" or type(k) == "boolean") then
-                    key = "["..tostring(k).."]"
-                else
-                    key = "['"..tostring(k).."']"
-                end
-  
-                if (type(v) == "number" or type(v) == "boolean") then
-                    output_str = output_str .. string.rep('\t',depth) .. key .. " = "..tostring(v)
-                elseif (type(v) == "table") then
-                    output_str = output_str .. string.rep('\t',depth) .. key .. " = {\n"
-                    table.insert(stack,node)
-                    table.insert(stack,v)
-                    cache[node] = cur_index+1
-                    break
-                else
-                    output_str = output_str .. string.rep('\t',depth) .. key .. " = '"..tostring(v).."'"
-                end
-  
-                if (cur_index == size) then
-                    output_str = output_str .. "\n" .. string.rep('\t',depth-1) .. "}"
-                else
-                    output_str = output_str .. ","
-                end
-            else
-                -- close the table
-                if (cur_index == size) then
-                    output_str = output_str .. "\n" .. string.rep('\t',depth-1) .. "}"
-                end
-            end
-  
-            cur_index = cur_index + 1
-        end
-  
-        if (size == 0) then
-            output_str = output_str .. "\n" .. string.rep('\t',depth-1) .. "}"
-        end
-  
-        if (#stack > 0) then
-            node = stack[#stack]
-            stack[#stack] = nil
-            depth = cache[node] == nil and depth + 1 or depth - 1
-        else
-            break
-        end
-    end
-  
-    -- This is necessary for working with HUGE tables otherwise we run out of memory using concat on huge strings
-    table.insert(output,output_str)
-    output_str = table.concat(output)
-  
-    print(output_str)
-end
-
 --------------------------------------------------------------------------------
 
 return lib
