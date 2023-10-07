a5603944e6d7be8a78df1b20174da765337416a6
omega-lua <140018816+omega-lua@users.noreply.github.com>
2023-10-07 15:10:02 +0200

    Update scene.lua

--- resources/scene/game/level1/scene.lua
+++ resources/scene/game/level1/scene.lua
@@ -1,14 +1,24 @@
 local composer = require( "composer" )
- 
 local scene = composer.newScene()
+
+local lib = require("resources.lib.lib")
+local dusk = require("Dusk.Dusk")
 
 -- -----------------------------------------------------------------------------------
 -- Scene variables
 -- -----------------------------------------------------------------------------------
 
+scene.type = 'game'
+scene.map = {}
+
 -- -----------------------------------------------------------------------------------
 -- Scene functions
 -- -----------------------------------------------------------------------------------
+
+local function onEnterFrame(event)
+    local map = scene.map
+    map.updateView() -- Tracks the camera to the focus and updates culling
+end
 
 -- -----------------------------------------------------------------------------------
 -- Scene event functions
@@ -16,7 +26,7 @@ local scene = composer.newScene()
  
 -- create()
 function scene:create( event )
- 
+
     local sceneGroup = self.view
     -- Code here runs when the scene is first created but has not yet appeared on screen
  
@@ -31,6 +41,11 @@ function scene:show( event )
  
     if ( phase == "will" ) then
         -- Code here runs when the scene is still off screen (but is about to come on screen)
+
+        -- insert into sceneGroup
+        local sceneGroup = scene.view
+        local map = scene.map
+        sceneGroup:insert(map)
  
     elseif ( phase == "did" ) then
         -- Code here runs when the scene is entirely on screen
@@ -72,5 +87,6 @@ scene:addEventListener( "show", scene )
 scene:addEventListener( "hide", scene )
 scene:addEventListener( "destroy", scene )
 -- -----------------------------------------------------------------------------------
- 
+Runtime:addEventListener( "enterFrame", onEnterFrame )
+
 return scene
