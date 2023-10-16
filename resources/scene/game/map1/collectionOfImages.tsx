09ef7dea4e0f2ee4bcd1b8f3cb8f728e52e8ec84
omega-lua <oliver@gomer.ch>
2023-10-16 22:07:56 +0200

    deleted unnecessary files

--- resources/scene/game/map1/collectionOfImages.tsx
+++
@@ -1,7 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.1" name="collectionOfImages" tilewidth="1473" tileheight="1441" tilecount="1" columns="0">
- <grid orientation="orthogonal" width="1" height="1"/>
- <tile id="0">
-  <image width="1473" height="1441" source="../../../graphics/player.png"/>
- </tile>
-</tileset>
